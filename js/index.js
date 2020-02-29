3957588fce00b30f43543746afd73e255052f389
Joanne Kus <kusjoanne@gmail.com>
2020-02-29 14:58:25 -0600

    jQuery practice

--- js/index.js
+++ js/index.js
@@ -11,3 +11,8 @@ today = mm + '/' + dd + '/' + yyyy;
 
 //will eventually need to be populated differently
 document.querySelector(".items-date").textContent = today;
+
+$("#addItem").click(function(){
+  console.log("Test2");
+  $("tbody").after("<tr><td>Rent</td><td>-1250.00</td><td>$15,000.00</td><td><img src='https://image.flaticon.com/icons/svg/1159/1159633.svg' class='edit'></td></tr>")
+});
