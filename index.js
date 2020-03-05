b049f8961e94d0f86e31e8cd017ad7aee7905a56
Joanne Kus <kusjoanne@gmail.com>
2020-03-05 14:34:34 -0600

    deploy test2

--- index.js
+++ index.js
@@ -38,18 +38,18 @@ $("input").change(function(){
 //settings Post
 
 //Edit Item
-
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-
-today = mm + '/' + dd + '/' + yyyy;
-
-//will eventually need to be populated differently
-document.querySelector(".items-date").textContent = today;
-
-$("#addItem").click(function(){
-  console.log("Test2");
-  $("tbody").after("<tr><td>Rent</td><td>-1250.00</td><td>$15,000.00</td><td><img src='https://image.flaticon.com/icons/svg/1159/1159633.svg' class='edit'></td></tr>");
-});
+// 
+// var today = new Date();
+// var dd = String(today.getDate()).padStart(2, '0');
+// var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+// var yyyy = today.getFullYear();
+//
+// today = mm + '/' + dd + '/' + yyyy;
+//
+// //will eventually need to be populated differently
+// document.querySelector(".items-date").textContent = today;
+//
+// $("#addItem").click(function(){
+//   console.log("Test2");
+//   $("tbody").after("<tr><td>Rent</td><td>-1250.00</td><td>$15,000.00</td><td><img src='https://image.flaticon.com/icons/svg/1159/1159633.svg' class='edit'></td></tr>");
+// });
