45e73f2b339a266ea01c9ec18edec8cef2008a48
jkus <kusjoanne@gmail.com>
2020-03-13 10:21:57 -0500

    reading items from mongo

--- index.js
+++ index.js
@@ -1,28 +1,74 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 const app = express();
+app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
+
 const port = process.env.PORT;
+const url = "mongodb+srv://admin-joanne:myMoon1204@cluster0-9ruej.mongodb.net/budgetAppDB?retryWrites=true&w=majority";
 
+mongoose.connect(url,{ useNewUrlParser: true });
 
-app.use(express.static("public"));
+const itemSchema = new mongoose.Schema({
+  name: String,
+  amount: Number
+});
+
+const dateSchema = new mongoose.Schema({
+  date: Date,
+  items: [itemSchema]
+});
+
+const Item = mongoose.model("Item",itemSchema);
+const ItemDate = mongoose.model("Date",dateSchema);
 
 app.listen(port||3000, function(){
   console.log("Listening on port "+port);
 });
 
+
 app.get("/", function(req,res){
-  res.sendFile(__dirname+"/index.html");
+  //query the items collection and lets just get all results
+  ItemDate.find(function(err, allDates){
+    if(allDates){
+      console.log(allDates);
+      res.render("main.ejs",{allDates:allDates});
+    }
+  })
+  // res.sendFile(__dirname+"/index.html");
 });
 
 //Add Item
 app.post("/",function(req,res){
   const itemName = req.body.itemName;
   const itemAmount = req.body.itemAmount;
-  const itemDate = req.body.itemDate;
-  console.log(itemName+" "+itemAmount+" "+itemDate);
+  const itemDate = new Date(req.body.itemDate).toISOString();
+  console.log("Passed Item Date From Form " + itemDate);
+  console.log(typeof itemDate);
+  const item = new Item({
+    name: itemName,
+    amount: itemAmount
+  })
+  item.save();
+
+  ItemDate.findOne({date:itemDate},function(err,itemsDate){
+    //if we already have items for that date just add to it
+    console.log("Date in DB "+itemsDate.date);
+    if(itemsDate){
+      itemsDate.items.push(item);
+    //if we don't create a new date
+    }else{
+      const date = new ItemDate({
+        date: itemDate,
+        items: [item]
+      });
+    }
+  });
+  res.redirect("/");
 });
 //
 // app.post("/",function(req,res){
@@ -33,10 +79,10 @@ app.post("/",function(req,res){
 //   console.log(itemName+" "+itemAmount+" "+itemDate+" "+itemFrequency);
 // });
 
-var today = new Date();
-var options = {weekday:'long', year:'numeric', month:'long', day:'numeric'};
-const todaysDate =today.toLocaleDateString("en-US",options);
-console.log(todaysDate);
+// var today = new Date();
+// var options = {weekday:'long', year:'numeric', month:'long', day:'numeric'};
+// const todaysDate =today.toLocaleDateString("en-US",options);
+// console.log(todaysDate);
 
 // will eventually need to be populated differently
 // document.querySelector(".items-date").textContent = today;
